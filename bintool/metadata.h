ce44160c44b8faced7e36e1de5d7b157fa4fcb86
Henry Gabryjelski <henrygab@users.noreply.github.com>
2024-12-30 18:01:57 -0800

    Fix: operation on 'i' may be undefined [-Wsequence-point]

--- bintool/metadata.h
+++ bintool/metadata.h
@@ -255,7 +255,8 @@ struct partition_table_item : public single_byte_size_item {
             new_p.flags = permissions_flags & (~PICOBIN_PARTITION_PERMISSIONS_BITS);
 
             if (new_p.flags & PICOBIN_PARTITION_FLAGS_HAS_ID_BITS) {
-                new_p.id = (uint64_t)data[i++] | ((uint64_t)data[i++] << 32);
+                new_p.id = (uint64_t)data[i] | ((uint64_t)data[i+1] << 32);
+                i += 2;
             }
 
             uint8_t num_extra_families = (new_p.flags & PICOBIN_PARTITION_FLAGS_ACCEPTS_NUM_EXTRA_FAMILIES_BITS) >> PICOBIN_PARTITION_FLAGS_ACCEPTS_NUM_EXTRA_FAMILIES_LSB;
